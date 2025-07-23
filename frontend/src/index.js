517558875bddd344e1b2bd040602de886e99354b
MayconBenvenuto <benvenutomaycon@gmail.com>
2025-07-23 11:33:38 -0300

    Adicionar @vercel/speed-insights como dependência e incluir componente no index.js; criar arquivo SENHA-CANCELAMENTO.md

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import AppFreePeriods from "./App-FreePeriods";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AppFreePeriods />
+    <SpeedInsights />
   </React.StrictMode>,
 );
